5ff2e783dbd4c14433ecd87959d8c40bef6db47d
WrexialMT <dylan.abela@gmail.com>
2026-02-02 01:13:26 +0100

    New minimap button

--- MinimapButton.lua
+++ MinimapButton.lua
@@ -1,4 +1,4 @@
--- HouseReview Minimap Button
+-- HouseReview Minimap Button using LibDataBroker
 
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("PLAYER_LOGIN")
@@ -7,75 +7,48 @@ frame:SetScript("OnEvent", function(self, event)
         self:UnregisterEvent("PLAYER_LOGIN")
 
         HouseReviewDB = HouseReviewDB or {}
-        HouseReviewDB.minimap = HouseReviewDB.minimap or { angle = 2.3 }
+        HouseReviewDB.minimap = HouseReviewDB.minimap or {}
 
-        local button = CreateFrame("Button", "HouseReviewMinimapButton", Minimap)
-        button:SetSize(32, 32)
-        button:SetFrameStrata("MEDIUM")
+        local ldb = LibStub:GetLibrary("LibDataBroker-1.1", true)
+        if not ldb then return end
 
-        local icon = button:CreateTexture(nil, "ARTWORK")
-        icon:SetAllPoints()
-        icon:SetTexture("Interface\\Icons\\inv_misc_house")
-        icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+        local dataobj = ldb:NewDataObject("HouseReview", {
+            type = "launcher",
+            icon = "Interface\\AddOns\\HouseReview\\Media\\minimap-icon.tga",
+            label = "House Review",
+            OnClick = function(_, button)
+                if button == "LeftButton" then
+                    if HouseReviewMapFrame:IsShown() then
+                        HouseReviewMapFrame:Hide()
+                    else
+                        local neighborhoodID
+                        if C_Housing and C_Housing.GetCurrentNeighborhood then
+                            neighborhoodID = C_Housing.GetCurrentNeighborhood()
+                        end
 
-        local border = button:CreateTexture(nil, "OVERLAY")
-        border:SetSize(53, 53)
-        border:SetPoint("CENTER")
-        border:SetTexture("Interface\\Minimap\\MiniMap-TrackingBorder")
+                        if not neighborhoodID then
+                            print("HouseReview: You must be in a housing neighborhood to show the map.")
+                            return
+                        end
+                        
+                        -- Cache the ID
+                        HouseReview.currentNeighborhoodID = neighborhoodID
 
-        button:SetScript("OnEnter", function(self)
-            GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
-            GameTooltip:AddLine("HouseReview")
-            GameTooltip:AddLine("|cFFDDDDDDClick to toggle the map.|r")
-            GameTooltip:Show()
-        end)
-        button:SetScript("OnLeave", function() GameTooltip:Hide() end)
-
-        button:RegisterForDrag("LeftButton")
-        button:SetScript("OnDragStart", function(self)
-            self:StartMoving()
-            self.isMoving = true
-        end)
-
-        button:SetScript("OnDragStop", function(self)
-            self:StopMovingOrSizing()
-            self.isMoving = false
-            
-            local minimapX, minimapY = Minimap:GetCenter()
-            local buttonX, buttonY = self:GetCenter()
-            
-            local angle = atan2(buttonY - minimapY, buttonX - minimapX)
-            HouseReviewDB.minimap.angle = angle
-            
-            local radius = 78
-            self:ClearAllPoints()
-            self:SetPoint("CENTER", Minimap, "CENTER", radius * cos(angle), radius * sin(angle))
-        end)
-
-        button:SetScript("OnClick", function(self)
-            if self.isMoving then return end
-            if HouseReviewMapFrame:IsShown() then
-                HouseReviewMapFrame:Hide()
-            else
-                local neighborhoodID
-                if C_Housing and C_Housing.GetCurrentNeighborhood then
-                    neighborhoodID = C_Housing.GetCurrentNeighborhood()
+                        HouseReview_UpdateMap()
+                        HouseReviewMapFrame:Show()
+                    end
                 end
-
-                if not neighborhoodID then
-                    print("HouseReview: You must be in a housing neighborhood to show the map.")
-                    return
-                end
-                
-                -- Cache the ID
-                HouseReview.currentNeighborhoodID = neighborhoodID
-
-                HouseReview_UpdateMap()
-                HouseReviewMapFrame:Show()
+            end,
+            OnTooltipShow = function(tooltip)
+                if not tooltip or not tooltip.AddLine then return end
+                tooltip:AddLine("HouseReview")
+                tooltip:AddLine("|cffeda55fClick|r to toggle the map.")
             end
-        end)
-        
-        local radius = 78
-        button:SetPoint("CENTER", Minimap, "CENTER", radius * cos(HouseReviewDB.minimap.angle), radius * sin(HouseReviewDB.minimap.angle))
+        })
+
+        local icon = LibStub("LibDBIcon-1.0", true)
+        if icon then
+            icon:Register("HouseReview", dataobj, HouseReviewDB.minimap)
+        end
     end
 end)
